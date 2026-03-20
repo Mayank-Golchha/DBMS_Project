0d74a5f00885a169d6944e63f4f7236410fc24ff
Mayank Golchha <mayank20044002@gmail.com>
2026-03-20 22:52:04 +0530

    added database to railway

--- db.js
+++ db.js
@@ -5,8 +5,20 @@
 
 const mysql = require("mysql2/promise");
 
+// const pool = mysql.createPool({
+//   host:               process.env.DB_HOST     || "localhost",
+//   user:               process.env.DB_USER     || "root",
+//   password:           process.env.DB_PASSWORD || "yourpassword",
+//   database:           process.env.DB_NAME     || "real_estate_db",
+//   waitForConnections: true,
+//   connectionLimit:    10,
+//   queueLimit:         0,
+// });
+
+
 const pool = mysql.createPool({
   host:               process.env.DB_HOST     || "localhost",
+  port:               process.env.DB_PORT     || 3306,       
   user:               process.env.DB_USER     || "root",
   password:           process.env.DB_PASSWORD || "yourpassword",
   database:           process.env.DB_NAME     || "real_estate_db",
